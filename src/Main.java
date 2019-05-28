7a2eb1788c856c7abe8cc751c7c8b346acc4c78b
XPS-9560\Dorian <popa.dorian98@gmail.com>
2019-05-28 19:52:24 +0300

    Successfully tested the Database connection
    Added testConnection() method to the Main class to execute a simple query

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String [] args) throws Exception{
+    public static void main(String [] args) throws Exception {
+        testConnection();
+    }
+
+    public static void testConnection() throws SQLException {
+        Connection con = Database.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("select id, nume from studenti");
+        while(rs.next()){
+            System.out.println(rs.getInt("ID") + " " + rs.getString("NUME"));
+        }
+        rs.close();
+        st.close();
+        Database.closeConnection();
     }
 }
